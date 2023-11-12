2ca007b8d0bae74c289224458c516ccf3b992dbc
evertonsouz4 <everton.souzaa@outlook.com.br>
2023-11-12 03:25:16 -0300

    Update

--- Lab03/lab03revisao1.cpp
+++ Lab03/lab03revisao1.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    double var1, var2, var3;                         
+    cout << "Digite 3 numeros para ser calculado o volume: ";
+    cin >> var1, cin >> var2, cin >> var3;
+    cout << "O volume e: " << var1 * var2 * var3;
+
+    return 0;
+}
