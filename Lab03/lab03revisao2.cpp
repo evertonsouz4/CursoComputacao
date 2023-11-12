473b1becaada6f698bd9a5b1f78ab0d2ef5bb08d
evertonsouz4 <everton.souzaa@outlook.com.br>
2023-11-12 03:46:59 -0300

    Update

---
+++ Lab03/lab03revisao2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int horas;
+    int minutos;
+
+    cout << "Digite a quantidade de horas: ";
+    cin >> horas;
+    cout << "Digite a quantidade de minutos: ";
+    cin >> minutos;
+    cout << "Sao " << horas << ":" << minutos << "\n\n";
+    cout << horas << " horas\n";
+    cout << minutos << " minutos\n";
+    
+    return 0;
+}
