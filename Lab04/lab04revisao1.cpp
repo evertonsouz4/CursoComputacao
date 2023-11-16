91c29f3d5789b5219aa1976e4b757d8f2cd2273c
evertonsouz4 <everton.souzaa@outlook.com.br>
2023-11-15 23:46:22 -0300

    Update

--- Lab04/lab04revisao1.cpp
+++ Lab04/lab04revisao1.cpp
@@ -1,1 +1,14 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
+int main() {
+    srand(2);
+    cout << rand() << "\n";
+    cout << rand() << "\n";
+    cout << rand() << "\n";
+    cout << rand() << "\n";
+    cout << rand() << "\n";
+
+    return 0;
+}
