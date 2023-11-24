6d6e7d44e706d0aeed01fef6bbcd274757346e8d
evertonsouz4 <everton.souzaa@outlook.com.br>
2023-11-24 19:21:24 -0300

    Update

--- Lab05/lab05aprendizagem5.cpp
+++ Lab05/lab05aprendizagem5.cpp
@@ -1,15 +1,20 @@
 #include <math.h>
 #include <iostream>
 using namespace std;
-double const PI = 3.141592;
+
+double arc_tan_graus(double y, double x);
 
 int main() {
-    double x, y, angl, result;
+    double x, y;
     cout << "Digite as coordenadas do vetor: \n";
     cout << "x: ";
     cin >> x;
     cout << "y: ";
     cin >> y;
-    cout << "\nO angulo do vetor e: " << result << " graus. ";
+    cout << "\nO angulo do vetor e: " << arc_tan_graus(y, x) << " graus. ";
     return 0;
+}
+
+double arc_tan_graus(double y, double x){
+    return atan2(y, x) * 180 / 3.141592;
 }
