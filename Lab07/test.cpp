064391297eab5b34ba174231fa8414a81ad25f56
Everton Souza <everton.souzaa@outlook.com.br>
2024-08-09 01:16:24 -0300

    Update test

--- Lab07/test.cpp
+++ Lab07/test.cpp
@@ -1,1 +1,12 @@
-teste
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num = 19;
+	int *num2 = &num;
+
+	cout << "O valor de num e: " << num << "\n";
+	cout << "O valor de num2 e: " << *num2 << "\n";
+
+	return 0;
+}
