b5e7b71d15f53ddcba0465500cb9b33f1971f4a6
Everton Souza <everton.souzaa@outlook.com.br>
2024-08-10 00:57:24 -0300

    Update

---
+++ Lab07/lab07fixacao1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+char convert_ascii(char num){
+	return num;
+}
+
+int main(){
+	 cout << convert_ascii(80);
+	 cout << convert_ascii(14);
+	 cout << convert_ascii(111);
+	 cout << convert_ascii(103);
+	 cout << convert_ascii(67);
+	 cout << convert_ascii(111);
+	 cout << convert_ascii(109);
+	 cout << convert_ascii(112) << "\n";
+
+
+	return 0;
+}
