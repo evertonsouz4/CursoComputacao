5985320e466903299c7182dbdacaad7eba3c20f5
Everton Souza <everton.souzaa@outlook.com.br>
2024-08-31 11:36:00 -0300

    lab07fixacao5.cpp

--- Lab07/lab07fixacao5.cpp
+++ Lab07/lab07fixacao5.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int bit;
-	cout << "Qual bit deseja ligar?: ";
-	cin >> bit;
+unsigned char ligarBit(unsigned char status, int posicao) {
+  unsigned char mascara = 1 << posicao;
+  return status = status | mascara;
+}
 
-	unsigned char mascara = 1 << bit;
-	unsigned char estado = 167;
-	estado = estado | mascara;
-	cout << int(estado);	
+unsigned char desligarBit(unsigned char status, int posicao) {
+  unsigned char mascara = ~(1 << posicao);
+  return status = status & mascara;
+}
 
-	return 0;
+bool testarBit(unsigned char status, int posicao) {
+  unsigned char mascara = 1 << posicao;
+  return status & mascara ? true : false;
+}
+
+int main() {
+  cout << int(ligarBit(167, 3)) << "\n";
+  cout << int(desligarBit(252, 3)) << "\n";
+  cout << testarBit(240, 7) << "\n";
+
+  return 0;
 }
